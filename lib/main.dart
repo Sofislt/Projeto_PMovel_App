d558430449cf24f775ef5a42dbfa07d8ca84ff66
Yasmim Weslla dos Santos <yws2@aluno.ifal.edu.br>
2025-08-22 16:47:46 -0300

    Adicionando tela de Estatísticas

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:projetofelype/Estatisticas.dart';
 import 'package:projetofelype/menuAvatar.dart';
+
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: menuAvatar(),
+      home: Estatisticas(),
     ),
   );
 }
