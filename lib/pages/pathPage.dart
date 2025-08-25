6f7b01fece822613ccbe6e53adf866a8e34285d1
Sofia Lima da Trindade <trindadelimasofia@gmail.com>
2025-08-25 14:21:02 -0300

    Iniciando a implementação do ListView.builder

--- lib/pages/pathPage.dart
+++ lib/pages/pathPage.dart
@@ -29,33 +29,14 @@ class _PathPageState extends State<PathPage> {
     return Expanded(
       child: TabBarView(
         children: [
-          ListView(
+          ListView.builder(
             padding: EdgeInsets.all(16.0),
-            children: [
-              Container(
-                width: 60,
-                height: 60,
-                margin: const EdgeInsets.only(top: 20.0,bottom: 40.0),
+            itemCount: Databas
+            buildTaskBullet();
 
-                decoration: BoxDecoration(
-                  color: Color(0xFF006A71),// border color
-                  shape: BoxShape.circle,
-                ),
-              ),
-              Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Color(0xFF006A71),// border color
-                  shape: BoxShape.circle,
-                ),
-              ),
-              ListTile(title: Text('Diário - Item 1')),
-              ListTile(title: Text('Diário - Item 2')),
-              ListTile(title: Text('Diário - Item 3')),
-              TaskCard(titulo: 'Ler o roteiro da feira literária', descricao: '17 páginas',),
-              TaskCard(titulo: 'Costurar saia jeans', descricao: '',),
-            ],
+
+            TaskCard(titulo: 'Ler o roteiro da feira literária', descricao: '17 páginas',),
+            TaskCard(titulo: 'Costurar saia jeans', descricao: '',),
           ),
           Center(child: Text("Diário Conteúdo")),
           Center(child: Text("Semanal Conteúdo")),
@@ -65,4 +46,17 @@ class _PathPageState extends State<PathPage> {
       ),
     );
   }
+}
+
+buildTaskBullet() {
+  return Container(
+    width: 60,
+    height: 60,
+    margin: const EdgeInsets.only(top: 20.0,bottom: 40.0),
+
+    decoration: BoxDecoration(
+      color: Color(0xFF006A71),// border color
+      shape: BoxShape.circle,
+    ),
+  );
 }
