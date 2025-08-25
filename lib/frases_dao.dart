b3a675d01a950a6612e324b72736a60d9b673c69
João <jpca2@aluno.ifal.edu.br>
2025-08-25 17:36:15 -0300

    atualização bdd

---
+++ lib/frases_dao.dart
@@ -0,0 +1,23 @@
+import 'package:projetofelype/dbhelper.dart';
+import 'package:projetofelype/frase.dart';
+import 'package:sqflite/sqflite.dart';
+
+class FrasesDao {
+  Future<List<Frase>> listarFrases() async {
+    Database db = await DBHelper().initDB();
+    String sql = 'SELECT * FROM Frases;';
+    var result = await db.rawQuery(sql);
+
+    List<Frase> listaFrases = [];
+
+    await Future.delayed(Duration(seconds: 3));
+
+    // ForEach
+    for (var json in result) {
+      Frase frases = Frase.fromJson(json);
+      listaFrases.add(frases);
+    }
+
+    return listaFrases;
+  }
+}
