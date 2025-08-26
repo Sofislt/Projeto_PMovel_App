8a3c738e574cf43b1ddbbfd1149c34a4feceae10
Yasmim <yws2@aluno.ifal.edu.br>
2025-08-26 10:46:31 -0300

    Coorreção do nome da tabela "Desempenho"

--- lib/DataBaseYasmim/DesempenhoDAO.dart
+++ lib/DataBaseYasmim/DesempenhoDAO.dart
@@ -5,7 +5,7 @@ import 'package:sqflite/sqflite.dart';
 class DesempenhosDao {
   Future<List<Desempenho>> listarDesempenhos() async {
     Database db = await DBHelper().initDB();
-    String sql = 'SELECT * FROM PROPRIEDADE;';
+    String sql = 'SELECT * FROM DESEMPENHO;';
     var result = await db.rawQuery(sql);
 
     List<Desempenho> listaDesempenhos = [];
