3d96b141ad102de36d973a9466e06335151675a9
Sofia Lima da Trindade <trindadelimasofia@gmail.com>
2025-10-21 12:07:48 -0300

    iniciando a implementação do gráfico de pizza

--- lib/Estatisticas.dart
+++ lib/Estatisticas.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:projetofelype/api/chart_api.dart';
+
+import 'Domain/chart.dart';
 
 class Estatisticas extends StatefulWidget {
   const Estatisticas({super.key});
@@ -9,6 +12,15 @@ class Estatisticas extends StatefulWidget {
 }
 
 class _EstatisticasState extends State<Estatisticas> {
+  late Future<Chart> futureChart;
+
+  @override
+  void initState() {
+    super.initState();
+    futureChart = ChartApi().get();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -22,12 +34,93 @@ class _EstatisticasState extends State<Estatisticas> {
           endDrawer: menuNavigationDrawer(),
           body: buildStatisticsBody(),
           extendBody: true,
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
           floatingActionButton: buildFloatingActionButton(),
           bottomNavigationBar: buildBottomAppBar(),
         ),
       ),
     );
+  }
+
+  buildAppBar() {
+    return AppBar(
+      //leading: IconButton(onPressed: () {}, icon: Icon(Icons.person_rounded),),
+      centerTitle: true,
+      backgroundColor: Color(0xFF005E65),
+      title: Text(
+        'App Name',
+        style: TextStyle(
+          fontSize: 26,
+          color: Colors.black,
+          fontWeight: FontWeight.w800,
+        ),
+      ),
+    );
+  }
+
+  buildFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      elevation: 0,
+      backgroundColor: Color(0xFF006A71),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32.0)),
+      foregroundColor: Color(0xFF000000),
+      child: const Icon(Icons.add, size: 40.0),
+    );
+  }
+
+  buildBottomAppBar() {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      color: Color(0xFF006A71),
+      child: IconTheme(
+        data: IconThemeData(),
+        child: Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                iconSize: 40.0,
+                onPressed: () {},
+                icon: Icon(Icons.home_filled),
+              ),
+              SizedBox(width: 24.0),
+              IconButton(
+                iconSize: 40.0,
+                onPressed: () {},
+                icon: Icon(Icons.bar_chart_rounded),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildStatisticsBody() {
+
+    return FutureBuilder(
+      future: futureChart,
+      builder: (context, snapshot) {
+        if(snapshot.hasData){
+          Chart chart = snapshot.requireData;
+          return Container(child: Image.network(chart.url));
+        }
+
+        if(snapshot.hasError){
+          return Center(child: Text('Um erro aconteceu!'));
+        }
+
+        return Center(child: CircularProgressIndicator());
+
+      },
+    );
+
+
+    // FUTURE BUILDER
+
   }
 }
 
@@ -38,37 +131,44 @@ class avatarNavigationDrawer extends StatelessWidget {
     child: SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          buildHeader(context),
-          buildMenuItems(context)
-        ],
+        children: <Widget>[buildHeader(context), buildMenuItems(context)],
       ),
     ),
   );
+
   Widget buildHeader(BuildContext context) => Container();
   Widget buildMenuItems(BuildContext context) => Container(
-      padding: const EdgeInsets.all(24),
-      child: Wrap(
-        runSpacing: 16,
-        children: [
-
-          ListTile(
-            leading: const Icon(Icons.person_rounded, color: Color(0xFFFFFFFF),),
-            title: const Text('Personalizar Avatar', style: TextStyle(color: Color(0xFFFFFFFF),),),
-            onTap: () {},
+    padding: const EdgeInsets.all(24),
+    child: Wrap(
+      runSpacing: 16,
+      children: [
+        ListTile(
+          leading: const Icon(Icons.person_rounded, color: Color(0xFFFFFFFF)),
+          title: const Text(
+            'Personalizar Avatar',
+            style: TextStyle(color: Color(0xFFFFFFFF)),
           ),
-          const Divider (color: Color(0xFFFFFFFF)),
-          ListTile(
-            leading: const Icon(Icons.local_grocery_store, color: Color(0xFFFFFFFF),),
-            title: const Text('Loja de Cosméticos', style: TextStyle(color: Color(0xFFFFFFFF),),),
-            onTap: () {},
+          onTap: () {},
+        ),
+        const Divider(color: Color(0xFFFFFFFF)),
+        ListTile(
+          leading: const Icon(
+            Icons.local_grocery_store,
+            color: Color(0xFFFFFFFF),
           ),
-        ],
-      )
+          title: const Text(
+            'Loja de Cosméticos',
+            style: TextStyle(color: Color(0xFFFFFFFF)),
+          ),
+          onTap: () {},
+        ),
+      ],
+    ),
   );
 }
 
-class menuNavigationDrawer extends StatelessWidget { //aqui o
+class menuNavigationDrawer extends StatelessWidget {
+  //aqui o
   @override
   Widget build(BuildContext context) => Drawer(
     backgroundColor: Color(0xFF9ACBD0),
@@ -79,7 +179,9 @@ class menuNavigationDrawer extends StatelessWidget { //aqui o
       ),
     ),
   );
+
   Widget buildHeader(BuildContext context) => Container();
+
   Widget buildMenuItems(BuildContext context) => Container(
     padding: const EdgeInsets.all(24),
     child: Wrap(
@@ -127,69 +229,4 @@ class menuNavigationDrawer extends StatelessWidget { //aqui o
       ],
     ),
   );
-}
-
-buildAppBar() {
-  return AppBar(
-    //leading: IconButton(onPressed: () {}, icon: Icon(Icons.person_rounded),),
-    centerTitle: true,
-    backgroundColor: Color(0xFF005E65),
-    title: Text(
-      'App Name',
-      style: TextStyle(
-        fontSize: 26,
-        color: Colors.black,
-        fontWeight: FontWeight.w800,
-      ),
-    ),
-  );
-}
-
-buildFloatingActionButton() {
-  return FloatingActionButton(
-    onPressed: () {},
-    elevation: 0,
-    backgroundColor: Color(0xFF006A71),
-    shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(32.0),
-    ),
-    foregroundColor: Color(0xFF000000),
-    child: const Icon(
-      Icons.add,
-      size: 40.0,
-    ),
-  );
-}
-
-buildBottomAppBar() {
-  return BottomAppBar(
-    shape: const CircularNotchedRectangle(),
-    color: Color(0xFF006A71),
-    child: IconTheme(
-      data: IconThemeData(),
-      child: Padding(
-        padding: const EdgeInsets.all(2.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              iconSize: 40.0,
-              onPressed: () {},
-              icon: Icon(Icons.home_filled),
-            ),
-            SizedBox(width: 24.0),
-            IconButton(
-              iconSize: 40.0,
-              onPressed: () {},
-              icon: Icon(Icons.bar_chart_rounded),
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-buildStatisticsBody() {
-
 }
