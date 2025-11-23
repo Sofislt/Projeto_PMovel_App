65d7396bbd0ce39ccbfcd5dee8923d65ab5c2f85
Sofia Lima da Trindade <trindadelimasofia@gmail.com>
2025-11-23 17:55:03 -0300

    Implementação da API verdadeira (lista de feriados)

--- lib/api/holidays_api.dart
+++ lib/api/holidays_api.dart
@@ -5,10 +5,20 @@ class HolidaysApi {
   final dio = Dio();
   String baseUrl = 'https://brasilapi.com.br/api';
 
-  Future<Holiday> findByYear(String year) async {
+  Future<List<Holiday>> findByYear(String year) async {
+    List<Holiday> listaHolidays = [];
+
     final response = await dio.get('$baseUrl/feriados/v1/$year');
 
-    Holiday listaHolidays = Holiday.fromJson(response.data);
+    if(response.statusCode == 200) {
+      var result = response.data;
+
+      //ForEach
+      for(var json in result) {
+        Holiday holiday = Holiday.fromJson(json);
+        listaHolidays.add(holiday);
+      }
+    }
     print(response);
     return listaHolidays;
   }
--- lib/pages/calendarPage.dart
+++ lib/pages/calendarPage.dart
@@ -43,68 +43,70 @@ class _CalendarPageState extends State<CalendarPage> {
   }
 
   loadData() async {
-    futureListaHoliday = HolidaysApi().findByYear(selectedYear);
+    setState(() {
+      futureListaHoliday = HolidaysApi().findByYear(selectedYear.toString());
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: buildCalendarBody(today, _onDaySelected),
+        child: buildCalendarBody(today, _onDaySelected, futureListaHoliday),
     );
   }
 }
 
-buildCalendarBody(today, _onDaySelected) {
+buildCalendarBody(today, _onDaySelected, futureListaHoliday) {
   return Padding(
     padding: const EdgeInsets.all(20.0),
-    child: Container(
-      child: ListView(
-        children: [
-          TableCalendar(
-            locale: "pt_BR",
-            rowHeight: 90,
-            headerStyle: HeaderStyle(
-              formatButtonVisible: false,
-              titleCentered: true,
-              titleTextFormatter: (date, locale) {
-                final formatted = DateFormat('MMMM yyyy', locale).format(date);
-                return formatted.capitalizeEachWord();
-              }
-            ),
-            availableGestures: AvailableGestures.all,
-            selectedDayPredicate: (day)=> isSameDay(day, today),
-            daysOfWeekStyle: DaysOfWeekStyle(
-              weekdayStyle: const TextStyle(fontWeight: FontWeight.w500),
-              weekendStyle: const TextStyle(fontWeight: FontWeight.w500),
-              dowTextFormatter: (date, locale) {
-                final text = DateFormat.E(locale).format(date);
-                return text.capitalizeEachWord();
-              },
-            ),
-            focusedDay: today,
-            firstDay: DateTime.utc(2024, 1, 1),
-            lastDay: DateTime.utc(2044, 12, 31),
-            onDaySelected: _onDaySelected,
+    child: Column(
+      children: [
+        TableCalendar(
+          locale: "pt_BR",
+          //rowHeight: 90,
+          headerStyle: HeaderStyle(
+            formatButtonVisible: false,
+            titleCentered: true,
+            titleTextFormatter: (date, locale) {
+              final formatted = DateFormat('MMMM yyyy', locale).format(date);
+              return formatted.capitalizeEachWord();
+            }
           ),
-          Container(
-            child: FutureBuilder<List<Holiday>>(
-              future: futureListaHoliday,
-              builder: (context, snapshot) {
-                if(snapshot.hasData) {
-                  List<Holiday> lista = snapshot.requireData;
-                  return buildListView(lista);
-                }
-                return Center(child: CircularProgressIndicator());
-              }
-            ),
+          availableGestures: AvailableGestures.all,
+          selectedDayPredicate: (day)=> isSameDay(day, today),
+          daysOfWeekStyle: DaysOfWeekStyle(
+            weekdayStyle: const TextStyle(fontWeight: FontWeight.w500),
+            weekendStyle: const TextStyle(fontWeight: FontWeight.w500),
+            dowTextFormatter: (date, locale) {
+              final text = DateFormat.E(locale).format(date);
+              return text.capitalizeEachWord();
+            },
           ),
-      ],),
+          focusedDay: today,
+          firstDay: DateTime.utc(2024, 1, 1),
+          lastDay: DateTime.utc(2044, 12, 31),
+          onDaySelected: _onDaySelected,
+        ),
+        Expanded(
+          child: FutureBuilder<List<Holiday>>(
+            future: futureListaHoliday,
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
+                List<Holiday> lista = snapshot.requireData;
+                return buildListView(lista);
+              }
+              return Center(child: CircularProgressIndicator());
+            }
+          ),
+        ),
+      ],
     ),
   );
 }
 
 buildListView(List<Holiday> listaHoliday) {
   return ListView.builder(
+    padding: const EdgeInsets.all(16.0),
     itemCount: listaHoliday.length,
     itemBuilder: (context, i) {
       return HolidayCard(
