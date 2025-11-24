8f8f4a168a87321ead7f50e2fd34d8684e604672
Sofia Lima da Trindade <trindadelimasofia@gmail.com>
2025-11-24 18:30:22 -0300

    Unindo a ConfigPage à MainPageController

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:projetofelype/pages/lojaRoupinhasPage.dart';
-import 'package:projetofelype/pages/configPage.dart';
 import 'package:projetofelype/pages/mainPageController.dart';
 
 void main() {
